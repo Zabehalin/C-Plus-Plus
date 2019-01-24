f19ce323da40a042f312bcd791d598e14832b9f1
Zabehalin Olexandr <Zabehalin@step.itstep.rv.ua>
2019-01-24 14:56:10 +0200

    DZ

---
+++ Class work/24.01.19/24.01.19/Source.cpp
@@ -0,0 +1,43 @@
+﻿/*1. Написати програму, яка виконує реверс елементів одновимірного масиву,
+розміщених між крайніми від’ємними числами у масиві. Для розв’язку задачі визначити та використати функції:
+- заповнення масиву
+- вивід масиву
+- пошук лівого від’ємного
+- пошук правого від’ємного у масиві
+- обертання частини масиву*/
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void Sum(int arr[], const int SIZE);
+
+void Print(int arr[], const int SIZE);
+
+int main()
+{
+	const int SIZE = 10;
+	int arr[SIZE];
+	Sum(arr, SIZE);
+	Print(arr, SIZE);
+
+	system("pause");
+	return 0;
+}
+
+void Sum(int arr[], const int SIZE)
+{
+	srand(unsigned(time(NULL)));
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % - 21 + 40;
+		
+	}
+}
+void Print(int arr[], const int SIZE)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "arr [" << i << "] = " << arr[i] << endl;
+	}
+}
