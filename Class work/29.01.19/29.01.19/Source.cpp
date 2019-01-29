63d0e774b5c70f2f8afa014ca2e0c2e5f37c221a
Zabehalin Olexandr <Zabehalin@step.itstep.rv.ua>
2019-01-29 13:15:29 +0200

    New Klass Worc

---
+++ Class work/29.01.19/29.01.19/Source.cpp
@@ -0,0 +1,24 @@
+﻿/*C++ 
+Написати функцію, яка приймає три аргументи: години, хвилини, секунди. 
+Функція повинна повертати еквівалент переданого їй тимчасового значення в секундах */
+
+#include <iostream>
+using namespace std;
+int Time(int G, int H, int S);
+int main()
+{
+	int G = 0, H = 0, S = 0;
+	cout << " Enter Hours = ";
+	cin >> G;
+	cout << " Enter Minutes = ";
+	cin >> H;
+	cout << " Enter Seconds = ";
+	cin >> S;
+	int SECONDS = Time(G, H, S);
+	cout << " Seconds = " << SECONDS << endl;
+	system("pause");
+}
+int Time(int G, int H, int S)
+{
+	return (G * 60 * 60) + (H * 60) + S;
+}
